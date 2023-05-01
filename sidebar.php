3b1ee2d49abfbf6092de1b34cd0a4242ef39cf90
rumyl <131506677+rumyl@users.noreply.github.com>
2023-05-02 01:39:53 +0800

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,133 @@
+
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.php" class="brand-link" style="text-align: center;">
+      <img src="nvc.png" height="100px" alt="NVC Logi"  style="opacity: .8">
+      <br>
+      <span class="brand-text font-weight-light">Human Resource <br> Management Information <br> System</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Programmer</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+         
+              
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <p>
+               Dashboard
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <p>
+               Registrations
+                <i class="fas fa-angle-left right"></i>
+                <span class="badge badge-info right">3</span>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="reg_employee.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Employee</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="reg_event.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Events</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="reg_period.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Payroll Period</p>
+                </a>
+              </li>
+              </a>
+            </ul>
+          </li>
+           <li class="nav-item">
+            <a href="#" class="nav-link">
+              <p>
+               Monitoring
+                <i class="fas fa-angle-left right"></i>
+                <span class="badge badge-info right">2</span>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="list_employee.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>List of Employees</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="timekeeping.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Timekeeping</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+
+     <li class="nav-item">
+            <a href="#" class="nav-link">
+              <p>
+               Reports
+                <i class="fas fa-angle-left right"></i>
+                <span class="badge badge-info right">2</span>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="report_1.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Timekeeping</p>
+                </a>
+              </li>
+             <li class="nav-item">
+                <a href="report_3.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Leave Summary</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+
+     <li class="nav-item">
+            <a href="settings.php" class="nav-link">
+              <p>
+               Settings
+              </p>
+            </a>
+          </li>         
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
